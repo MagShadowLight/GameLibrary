3a25b0e8c72b42d00cf1395b785ccb708de9b5ed
MagShadowLight <williamrose1025@icloud.com>
2026-01-11 18:43:39 -0800

    inital commit

---
+++ GameLibraryData/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibraryData
+{
+    public interface IDataAccess<T>
+    {
+        public List<T> GetAll();
+
+        public T GetById(int id);
+    }
+}
